6861d8695ea3fac3ecf5049133b463e5c9026205
y9hack337 <saveliev.tut42@gmail.com>
2024-09-24 00:33:23 +0300

    мяу

--- TMessagesProj/src/main/java/uz/unnarsx/komarugram/preferences/AboutPreferencesEntry.kt
+++ TMessagesProj/src/main/java/uz/unnarsx/komarugram/preferences/AboutPreferencesEntry.kt
@@ -1,54 +1,54 @@
-package uz.unnarsx.cherrygram.preferences
+package uz.unnarsx.komarugram.preferences
 
 import android.os.Bundle
 import org.telegram.messenger.AndroidUtilities
 import org.telegram.messenger.BuildConfig
 import org.telegram.messenger.BuildVars
-import org.telegram.messenger.LocaleController.getString
+import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import org.telegram.messenger.browser.Browser
 import org.telegram.ui.ActionBar.BaseFragment
 import org.telegram.ui.ChatActivity
 import org.telegram.ui.Components.BulletinFactory
-import uz.unnarsx.cherrygram.core.configs.CherrygramCoreConfig
-import uz.unnarsx.cherrygram.Extra
-import uz.unnarsx.cherrygram.core.crashlytics.Crashlytics
-import uz.unnarsx.cherrygram.misc.Constants
-import uz.unnarsx.cherrygram.core.helpers.CGResourcesHelper
-import uz.unnarsx.cherrygram.preferences.tgkit.CherrygramPreferencesNavigator
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.category
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.textDetail
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.textIcon
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.tgKitScreen
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.types.TGKitTextDetailRow
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.types.TGKitTextIconRow
-import uz.unnarsx.cherrygram.core.updater.UpdaterBottomSheet
-import uz.unnarsx.cherrygram.core.updater.UpdaterUtils
+import uz.unnarsx.komarugram.komaruGramConfig
+import uz.unnarsx.komarugram.Extra
+import uz.unnarsx.komarugram.core.crashlytics.Crashlytics
+import uz.unnarsx.komarugram.misc.Constants
+import uz.unnarsx.komarugram.core.helpers.CGResourcesHelper
+import uz.unnarsx.komarugram.preferences.tgkit.komaruGramPreferencesNavigator
+import uz.unnarsx.komarugram.preferences.tgkit.preference.category
+import uz.unnarsx.komarugram.preferences.tgkit.preference.textDetail
+import uz.unnarsx.komarugram.preferences.tgkit.preference.textIcon
+import uz.unnarsx.komarugram.preferences.tgkit.preference.tgKitScreen
+import uz.unnarsx.komarugram.preferences.tgkit.preference.types.TGKitTextDetailRow
+import uz.unnarsx.komarugram.preferences.tgkit.preference.types.TGKitTextIconRow
+import uz.unnarsx.komarugram.core.updater.UpdaterBottomSheet
+import uz.unnarsx.komarugram.core.updater.UpdaterUtils
 
 class AboutPreferencesEntry : BasePreferencesEntry {
-    override fun getPreferences(bf: BaseFragment) = tgKitScreen(getString(R.string.CGP_Header_About)) {
-        category(getString(R.string.Info)) {
+    override fun getPreferences(bf: BaseFragment) = tgKitScreen(LocaleController.getString("CGP_Header_About", R.string.CGP_Header_About)) {
+        category(LocaleController.getString("Info", R.string.Info)) {
             textDetail {
                 title = CGResourcesHelper.getAppName() + " " + Constants.CG_VERSION + " | " + "Telegram v" + BuildVars.BUILD_VERSION_STRING
-                detail = getString(R.string.CGP_About_Desc)
+                detail = LocaleController.getString("CGP_About_Desc", R.string.CGP_About_Desc)
 
                 listener = TGKitTextDetailRow.TGTDListener {
-                    Browser.openUrl(bf.parentActivity, "https://github.com/arsLan4k1390/Cherrygram#readme")
+                    Browser.openUrl(bf.parentActivity, "https://github.com/hack337-LZT/komaruGram")
                 }
             }
 
             textDetail {
                 icon = R.drawable.sync_outline_28
-                title = getString(R.string.UP_Category_Updates)
+                title = LocaleController.getString("UP_Category_Updates", R.string.UP_Category_Updates)
                 detail = UpdaterUtils.getLastCheckUpdateTime()
 
                 listener = TGKitTextDetailRow.TGTDListener {
-                    if (CherrygramCoreConfig.isPlayStoreBuild()) {
-                        CherrygramCoreConfig.lastUpdateCheckTime = System.currentTimeMillis()
+                    if (komaruGramConfig.isPlayStoreBuild()) {
+                        komaruGramConfig.lastUpdateCheckTime = System.currentTimeMillis()
                         detail = UpdaterUtils.getLastCheckUpdateTime()
 
                         Browser.openUrl(bf.context, Extra.PLAYSTORE_APP_URL)
-                    } else if (CherrygramCoreConfig.isStandalonePremiumBuild()) {
+                    } else if (komaruGramConfig.isPremiumBuild()) {
                         // Fuckoff :)
                     } else {
                         UpdaterBottomSheet.showAlert(bf.context, bf, false, null)
@@ -58,11 +58,11 @@ class AboutPreferencesEntry : BasePreferencesEntry {
 
             textIcon {
                 icon = R.drawable.bug_solar
-                title = getString(R.string.CG_CopyReportDetails)
+                title = LocaleController.getString("CG_CopyReportDetails", R.string.CG_CopyReportDetails)
 
                 listener = TGKitTextIconRow.TGTIListener {
                     AndroidUtilities.addToClipboard(Crashlytics.getReportMessage().toString() + "\n\n#bug")
-                    BulletinFactory.of(bf).createErrorBulletin(getString(R.string.CG_ReportDetailsCopied)).show()
+                    BulletinFactory.of(bf).createErrorBulletin(LocaleController.getString("CG_ReportDetailsCopied", R.string.CG_ReportDetailsCopied)).show()
                 }
             }
             textIcon {
@@ -70,77 +70,78 @@ class AboutPreferencesEntry : BasePreferencesEntry {
                 title = "Debug // WIP"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    it.presentFragment(CherrygramPreferencesNavigator.createDebug())
+                    it.presentFragment(komaruGramPreferencesNavigator.createDebug())
                 }
             }
         }
 
-        category(getString(R.string.CGP_Links)) {
+        category(LocaleController.getString("CGP_Links", R.string.CGP_Links)) {
             textIcon {
                 icon = R.drawable.msg_channel_solar
-                title = getString(R.string.CGP_ToChannel)
-                value = "@Cherry_gram"
+                title = LocaleController.getString("CGP_ToChannel", R.string.CGP_ToChannel)
+                value = "@komaruGram"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    Browser.openUrl(bf.parentActivity, "https://t.me/Cherry_gram")
+                    Browser.openUrl(bf.parentActivity, "https://t.me/komarugram")
                 }
             }
             textIcon {
                 icon = R.drawable.msg_discuss_solar
-                title = getString(R.string.CGP_ToChat)
-                value = "@CherrygramSupport"
+                title = LocaleController.getString("CGP_ToChat", R.string.CGP_ToChat)
+                value = "@komaruGramChat"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    Browser.openUrl(bf.parentActivity, "https://t.me/CherrygramSupport")
+                    Browser.openUrl(bf.parentActivity, "https://t.me/komarugram")
                 }
             }
-            textIcon {
-                isAvailable = !CherrygramCoreConfig.isStandalonePremiumBuild()
-                icon = R.drawable.github_logo_white
-                title = getString(R.string.CGP_Source)
+            if (!komaruGramConfig.isPremiumBuild()) {
+                textIcon {
+                    icon = R.drawable.github_logo_white
+                    title = LocaleController.getString("CGP_Source", R.string.CGP_Source)
 
-                value = if (CherrygramCoreConfig.isStandaloneBetaBuild() || CherrygramCoreConfig.isDevBuild()) {
-                    "GitHub"
-                } else {
-                    "commit " + BuildConfig.GIT_COMMIT_HASH.substring(0, 8)
-                }
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    if (CherrygramCoreConfig.isStandaloneBetaBuild() || CherrygramCoreConfig.isDevBuild()) {
-                        Browser.openUrl(bf.parentActivity, "https://github.com/arsLan4k1390/Cherrygram/")
+                    value = if (komaruGramConfig.isBetaBuild() || komaruGramConfig.isDevBuild()) {
+                        "GitHub"
                     } else {
-                        Browser.openUrl(bf.parentActivity, "https://github.com/arsLan4k1390/Cherrygram/commit/" + BuildConfig.GIT_COMMIT_HASH)
+                        "commit " + BuildConfig.GIT_COMMIT_HASH
+                    }
+
+                    listener = TGKitTextIconRow.TGTIListener {
+                        if (komaruGramConfig.isBetaBuild() || komaruGramConfig.isDevBuild()) {
+                            Browser.openUrl(bf.parentActivity, "https://github.com/y9hack337/komaruGram/")
+                        } else {
+                            Browser.openUrl(bf.parentActivity, "https://github.com/y9hack337/komaruGram/commit/" + BuildConfig.GIT_COMMIT_HASH)
+                        }
                     }
                 }
             }
             textIcon {
                 icon = R.drawable.msg_translate_solar
-                title = getString(R.string.CGP_Crowdin)
+                title = LocaleController.getString("CGP_Crowdin", R.string.CGP_Crowdin)
                 value = "Crowdin"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    Browser.openUrl(bf.parentActivity, "https://crowdin.com/project/cherrygram")
+                    Browser.openUrl(bf.parentActivity, "https://crowdin.com/")
                 }
             }
 
-            if (CherrygramCoreConfig.isPlayStoreBuild()) {
+            if (komaruGramConfig.isPlayStoreBuild()) {
                 textIcon {
                     icon = R.drawable.msg2_policy
-                    title = getString(R.string.PrivacyPolicy)
+                    title = LocaleController.getString("PrivacyPolicy", R.string.PrivacyPolicy)
 
                     listener = TGKitTextIconRow.TGTIListener {
-                        Browser.openUrl(bf.parentActivity, "https://arslan4k1390.github.io/cherrygram/privacy")
+                        Browser.openUrl(bf.parentActivity, "https://github.com/y9hack337/komaruGram")
                     }
                 }
             }
-            /*textIcon {
+            textIcon {
                 icon = R.drawable.heart_angle_solar
-                title = getString(R.string.DP_Donate)
+                title = LocaleController.getString("DP_Donate", R.string.DP_Donate)
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    it.presentFragment(CherrygramPreferencesNavigator.createDonate())
+                    it.presentFragment(komaruGramPreferencesNavigator.createDonate())
                 }
-            }*/
+            }
         }
 
     }
--- TMessagesProj/src/main/java/uz/unnarsx/komarugram/preferences/DonatePreferenceEntry.kt
+++ TMessagesProj/src/main/java/uz/unnarsx/komarugram/preferences/DonatePreferenceEntry.kt
@@ -1,136 +1,41 @@
-package uz.unnarsx.cherrygram.preferences
+package uz.unnarsx.komarugram.preferences
 
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
 import org.telegram.messenger.AndroidUtilities
-import org.telegram.messenger.LocaleController.getString
+import org.telegram.messenger.LocaleController
 import org.telegram.messenger.R
 import org.telegram.ui.ActionBar.BaseFragment
 import org.telegram.ui.ActionBar.Theme
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.category
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.textIcon
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.tgKitScreen
-import uz.unnarsx.cherrygram.preferences.tgkit.preference.types.TGKitTextIconRow
+import uz.unnarsx.komarugram.preferences.tgkit.preference.category
+import uz.unnarsx.komarugram.preferences.tgkit.preference.textIcon
+import uz.unnarsx.komarugram.preferences.tgkit.preference.tgKitScreen
+import uz.unnarsx.komarugram.preferences.tgkit.preference.types.TGKitTextIconRow
 
 class DonatePreferenceEntry : BasePreferencesEntry {
-    override fun getPreferences(bf: BaseFragment) = tgKitScreen(getString(R.string.DP_Donate)) {
+    override fun getPreferences(bf: BaseFragment) = tgKitScreen(LocaleController.getString("DP_Donate", R.string.DP_Donate)) {
         val isDarkMode: Boolean = !Theme.isCurrentThemeDay()
-        category(getString(R.string.DP_Donate_Method)) {
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_visa_dark else R.drawable.card_visa_light
-                title = "VISA (Visa Direct)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("4278310028377794")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_master_dark else R.drawable.card_master_light
-                title = "MasterCard (MoneySend)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("5477330021782747")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_tirikchilik_dark else R.drawable.card_tirikchilik_light
-                title = "Tirikchilik (Uzbekistan)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    val openURL = Intent(Intent.ACTION_VIEW)
-                    openURL.data = Uri.parse("https://tirikchilik.uz/arslan4k1390")
-                    bf.parentActivity.startActivity(openURL)
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_humo_dark else R.drawable.card_humo_light
-                title = "HUMO (Uzbekistan)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("9860100124035617")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_uzcard_dark else R.drawable.card_uzcard_light
-                title = "UzCard (Uzbekistan)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("8600490727700080")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_uzcard_mir_dark else R.drawable.card_uzcard_mir_light
-                title = "UzCard-MIR Co-Badge (Uzbekistan)"
-                divider = true
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("5614681912473893")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
+        category(LocaleController.getString("DP_Donate_Method", R.string.DP_Donate_Method)) {
             textIcon {
                 icon = if (isDarkMode) R.drawable.card_ym_dark else R.drawable.card_ym_light
                 title = "YooMoney (RUB)"
 
                 listener = TGKitTextIconRow.TGTIListener {
                     AndroidUtilities.addToClipboard("4100116983696293")
-                    Toast.makeText(bf.parentActivity, getString(R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-
-        category("Binance") {
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_binance_dark else R.drawable.card_binance_light
-                title = "Binance ID"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("220943480")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_btc_dark else R.drawable.card_btc_light
-                title = "Bitcoin (BTC)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("1Pr6GqqWakgKWW1nDjVyHUYo1AcWbSN453")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_eth_dark else R.drawable.card_eth_light
-                title = "Ethereum (ERC20)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("0xa8392346f426443ef7e3d98047bace6dbcc0e7d8")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
-                }
-            }
-            textIcon {
-                icon = if (isDarkMode) R.drawable.card_usdt_dark else R.drawable.card_usdt_light
-                title = "TetherUS - USDT (TRC20)"
-
-                listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("TKnPT5rojMf851ejov2Xu4pxKcMfSh4Ws9")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("CardNumberCopied", R.string.CardNumberCopied), Toast.LENGTH_SHORT).show()
                 }
             }
         }
         category("Telegram Wallet") {
             textIcon {
                 icon = if (isDarkMode) R.drawable.card_ton_dark else R.drawable.card_ton_light
-                title = "TON Coin (v3R2) // Tonkeeper"
+                title = "TON Coin // Tonkeeper"
                 divider = true
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("EQCVVayzZkpm4LoHi9yuJQFjxRqi2qM4o0dqMLvZnXOFFE-3")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    AndroidUtilities.addToClipboard("UQBa1XcnAEjwQmSUsyvhFsy3GtVcqOIgI8b7YvYI8-buIGpv")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
                 }
             }
             textIcon {
@@ -138,8 +43,8 @@ class DonatePreferenceEntry : BasePreferencesEntry {
                 title = "TON Coin // @wallet"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("UQBDb7Px1bDnNyGniB38ZjW3tx2kP3cQL7VcF9XzWvMS3sAa")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    AndroidUtilities.addToClipboard("UQBa1XcnAEjwQmSUsyvhFsy3GtVcqOIgI8b7YvYI8-buIGpv")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
                 }
             }
             textIcon {
@@ -147,8 +52,8 @@ class DonatePreferenceEntry : BasePreferencesEntry {
                 title = "NOT Coin // @wallet"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("UQBDb7Px1bDnNyGniB38ZjW3tx2kP3cQL7VcF9XzWvMS3sAa")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    AndroidUtilities.addToClipboard("UQBa1XcnAEjwQmSUsyvhFsy3GtVcqOIgI8b7YvYI8-buIGpv")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
                 }
             }
             textIcon {
@@ -156,8 +61,17 @@ class DonatePreferenceEntry : BasePreferencesEntry {
                 title = "USDT (TRC20) // @wallet"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("TU61MLJEcQqXCqqvAk651kPieCdUSMsjvf")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    AndroidUtilities.addToClipboard("TRLCEZBWPPnyeYXteb6Nw6rMqtyprsQkZi")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                }
+            }
+            textIcon {
+                icon = if (isDarkMode) R.drawable.card_usdt_dark else R.drawable.card_usdt_light
+                title = "USDT (BEP20) // @wallet"
+
+                listener = TGKitTextIconRow.TGTIListener {
+                    AndroidUtilities.addToClipboard("0x165829AF97323f80A6B2B9fa2f0c27366595B29E")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
                 }
             }
             textIcon {
@@ -165,8 +79,8 @@ class DonatePreferenceEntry : BasePreferencesEntry {
                 title = "Bitcoin (BTC) // @wallet"
 
                 listener = TGKitTextIconRow.TGTIListener {
-                    AndroidUtilities.addToClipboard("1FsXkKKCpPF5EszZCd4KwhJhjGSjmwzqQZ")
-                    Toast.makeText(bf.parentActivity, getString(R.string.TextCopied), Toast.LENGTH_SHORT).show()
+                    AndroidUtilities.addToClipboard("bc1qhda7ss7spdrpg8vnzv6elwpsuwhvdvy5uy5yj8")
+                    Toast.makeText(bf.parentActivity, LocaleController.getString("TextCopied", R.string.TextCopied), Toast.LENGTH_SHORT).show()
                 }
             }
 
